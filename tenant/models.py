700ce0748139fc350ade71fdc010c11c611e33ba
tenisancms <tambora.the@gmail.com>
2022-07-21 12:09:06 +0530

    first commit

---
+++ tenant/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django_tenants.models import DomainMixin, TenantMixin
+
+
+class Tenant(TenantMixin):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # featured = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+
+    is_active = models.BooleanField(default=False, blank=True)
+    created_on = models.DateField(auto_now_add=True)
+
+    # default true, schema will be automatically created and
+    # synced when it is saved
+    auto_create_schema = True
+
+    """
+    USE THIS WITH CAUTION!
+    Set this flag to true on a parent class if you want the schema to be
+    automatically deleted if the tenant row gets deleted.
+    """
+    auto_drop_schema = True
+
+
+
+
+    def __str__(self):
+        return f"{self.schema_name}"
+
+
+class Domain(DomainMixin):
+    pass
